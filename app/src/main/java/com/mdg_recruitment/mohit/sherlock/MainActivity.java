1daaac3ee134ad498aa9fb83968652caa476413d
Mohit Jain <mohitjain1999@yahoo.com>
2016-12-24 16:46:41 +0530

    Add list of seasons

--- app/src/main/java/com/mdg_recruitment/mohit/sherlock/MainActivity.java
+++ app/src/main/java/com/mdg_recruitment/mohit/sherlock/MainActivity.java
@@ -2,6 +2,9 @@ package com.mdg_recruitment.mohit.sherlock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Array of String storing seasons
+        String[] seasonArray = {"Season 1", "Season 2", "Season 3", "Season 4"};
+        //Creates ListAdapter to convert String to List items
+        ListAdapter seasonAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, seasonArray);
+        //Create ListView object
+        ListView seasonListView = (ListView) findViewById(R.id.seasons);
+        //Add values to list
+        seasonListView.setAdapter(seasonAdapter);
     }
 }
