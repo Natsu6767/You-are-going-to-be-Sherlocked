6d8203b3098b979e9fe0ef7741ca28d9b2b3dc8a
Mohit Jain <mohitjain1999@yahoo.com>
2016-12-26 22:56:38 +0530

    Add text to each season activity

--- app/src/main/java/com/mdg_recruitment/mohit/sherlock/Season2Activity.java
+++ app/src/main/java/com/mdg_recruitment/mohit/sherlock/Season2Activity.java
@@ -2,6 +2,9 @@ package com.mdg_recruitment.mohit.sherlock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class Season2Activity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Season2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_season2);
+
+        //Array of String storing seasons
+        String[] episodeArray = {"Episode 1", "Episode 2", "Episode 3"};
+        //Creates ListAdapter to convert String to List items
+        ListAdapter seasonAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, episodeArray);
+        //Create ListView object
+        ListView season2ListView = (ListView) findViewById(R.id.season2);
+        //Add values to list
+        season2ListView.setAdapter(seasonAdapter);
     }
 }
--- app/src/main/java/com/mdg_recruitment/mohit/sherlock/Season4Activity.java
+++ app/src/main/java/com/mdg_recruitment/mohit/sherlock/Season4Activity.java
@@ -2,6 +2,9 @@ package com.mdg_recruitment.mohit.sherlock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class Season4Activity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Season4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_season4);
+
+        //Array of String storing seasons
+        String[] episodeArray = {"Episode 0", "Episode 1", "Episode 2", "Episode 3"};
+        //Creates ListAdapter to convert String to List items
+        ListAdapter seasonAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, episodeArray);
+        //Create ListView object
+        ListView season4ListView = (ListView) findViewById(R.id.season4);
+        //Add values to list
+        season4ListView.setAdapter(seasonAdapter);
     }
 }
