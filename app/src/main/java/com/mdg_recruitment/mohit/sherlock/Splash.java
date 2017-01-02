5a656cb4b63d9eae5b1feee9852d596316911c46
Mohit Jain <mohitjain1999@yahoo.com>
2017-01-03 02:27:22 +0530

    Change android image icon

--- app/src/main/java/com/mdg_recruitment/mohit/sherlock/Splash.java
+++ app/src/main/java/com/mdg_recruitment/mohit/sherlock/Splash.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 public class Splash extends Activity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 4250;
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
 
     /** Called when the activity is first created. */
     @Override
